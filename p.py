0079cba0d2f411e8d60d96d1f064c11f56244ba1
BruceLeekaiyu <531047675@qq.com>
2016-03-29 21:31:38 +0800

    Coding gene.....

--- p.py
+++ p.py
@@ -2,36 +2,62 @@
 import string
 import math
 import random
+import numpy as np
+import matplotlib.pyplot as plt
 
-def isPrime(n):
-	if n<=1:
-		return False
-	for i in range(2,int(math.sqrt(n))+1):
-		if n % i == 0:
-			return False
-	return True
+DIVIDE = 10
 
-def num(n):
-	if isPrime(n):
-		return int(random.uniform(50,1000))
-	else:
-		if n%2==0:
-			return int(random.uniform(1000,5000))
-		else:
-			return int(random.uniform(1000,5000))+5000
+class DataSlice(object):
+
+	def __init__(self ,Data ,range,threshold,size):
+		self.DataList = Data
+		self.begin = range[0]
+		self.end = range[1]
+		self.mean = np.mean(Data)
+		self.std = np.var(Data)
+		self.Sample = []
+		self.threshold = threshold
+		self.size = size
+
+	def __getDistance__(self, p, polyPara):
+		t = len(polyPara)
+		res = 0
+		time = 1
+		for i in range(t):
+			res = res + time *polyPara[-1]
+			time = time * p[0]
+
+		return abs(p[1] - res)
+
+	def __polyfit__(self,L):
+		#self.DataList.sort()
+		Len = len(L)
+		x_degree = range(1,(Len+1))
+		p = np.polyfit(x_degree,L,3)
+		return p
+
+	def __GeneOrder__(self):
+		self.DataList.sort()
+		PriorList = []
+		PriorList.append(self.DataList[0])
+		self.DataList.pop(0)
+		priorList.append(self.DataList[-1])
+		self.DataList.pop()
+
+	def __GeneSample__(self):
+		Len = len(self.DataList)
+		bottom = int(Len*self.begin)
+		top = int(Len*self.bottom)
+
+	def __PrintTest__(self):
+		print self.DataList 
+		print self.Sample 
+
 
 if __name__ == '__main__':
-	Qcs = open("QCS.csv")
-	data = open("data.csv",'w')
-	cnt = 0
-	for each_line in Qcs:
-		cnt = cnt + 1
-		each_line = each_line.strip("\n")
-		Size = num(cnt)
-		for i in range(Size):
-			data.write(each_line)
-			data.write(',')
-			data.write(str(int(random.uniform(0,4294967295))))
-			data.write('\n')
-	data.close()
-	Qcs.close()
+	L = [1,23,2,3,4,5,6,6,78,9,9]
+	t = [0.1,0.5]
+	DN = DataSlice(L,t)
+
+	DN.__PrintTest__()
+	print L
